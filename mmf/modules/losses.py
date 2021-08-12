bdc42ab7d47fecc37e7661e70c5188d227cb1c32
WesRobbins <wrbigsky@gmail.com>
2021-08-12 00:03:29 +0000

    add gp loss

--- mmf/modules/losses.py
+++ mmf/modules/losses.py
@@ -576,6 +576,53 @@ class M4CDecodingBboxPenalty(nn.Module):
         loss = torch.sum(losses) / count
         return loss
 
+from sys import exit
+@registry.register_loss("gp_mdbce")
+class GPLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.one = torch.Tensor([1.0])
+
+    def forward(self, sample_list, model_output):
+        scores = model_output["scores"]
+        targets = sample_list["targets"]
+        print(scores)
+        print(targets)
+        exit()
+        loss_mask = sample_list["train_loss_mask"]
+        assert scores.dim() == 3 and loss_mask.dim() == 2
+
+        losses = F.binary_cross_entropy_with_logits(scores, targets, reduction="none")
+        losses *= loss_mask.unsqueeze(-1)
+
+        count = torch.max(torch.sum(loss_mask), self.one.to(losses.device))
+        loss = torch.sum(losses) / count
+        return loss
+
+from sys import exit
+@registry.register_loss("gp_mdbce")
+class GPLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.one = torch.Tensor([1.0])
+
+    def forward(self, sample_list, model_output):
+        scores = model_output["scores"]
+        targets = sample_list["targets"]
+        print(scores)
+        print(targets)
+        exit()
+        loss_mask = sample_list["train_loss_mask"]
+        assert scores.dim() == 3 and loss_mask.dim() == 2
+
+        losses = F.binary_cross_entropy_with_logits(scores, targets, reduction="none")
+        losses *= loss_mask.unsqueeze(-1)
+
+        count = torch.max(torch.sum(loss_mask), self.one.to(losses.device))
+        loss = torch.sum(losses) / count
+        return loss
+
+
 
 @registry.register_loss("m4c_decoding_bce_with_mask")
 class M4CDecodingBCEWithMaskLoss(nn.Module):
